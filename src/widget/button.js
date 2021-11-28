a0235bda8dc219cd6ebfd23344d4e66ebb42f61c
infinityworld13 <teresita.becerra@mail.utoronto.ca>
2021-11-28 14:09:42 -0500

    Added Clean architecture base in order to refractor widget

---
+++ src/widget/button.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Button } from 'antd';
+
+class Buttons extends React.Component {
+    constructor(props) {
+        super();
+        this.props = props;
+    }
+
+
+    render() {
+        return (
+            <Button
+                type="primary"
+            > {this.props.name}
+            </Button>
+        );
+    }
+}
+export default Buttons;
